e6eea155e2c43b92c72e297775c492f46c581650
Brett Hardy <brett8hardy@gmail.com>
2025-03-18 09:19:53 -0700

    Made first person camera and movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private Camera cam;
+    [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float jump;
+
+    private float jumpRay;
+    private Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jumpRay = transform.localScale.y + 0.05f;
+
+        inputManager.OnMove.AddListener(Move);
+        inputManager.OnSpacePressed.AddListener(Jump);
+        inputManager.OnMousePressed.AddListener(Shoot);
+    }
+
+    void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, cam.transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z); 
+        Debug.DrawRay(cam.transform.position, cam.transform.forward*5f,Color.yellow);
+        rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, maxSpeed);
+    }
+
+    // Update is called once per frame
+    private void Move(Vector2 direction)
+    {
+        Vector3 moveDirection = rb.rotation * new Vector3(direction.x,0f,direction.y);
+        rb.AddForce(speed * moveDirection, ForceMode.Impulse);
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded())
+        {
+            rb.AddForce(jump * Vector3.up, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, jumpRay))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void Shoot()
+    {
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit, Mathf.Infinity))
+        {
+        }
+    }
+}
